22e1cd1310840664a45dedff8e925543fd6b245b
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-12 16:18:29 -0300

    Revert: Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt()],
   site: 'https://portfolio-tiagoalvarezschiaffino.netlify.app/'
 })
